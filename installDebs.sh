2cc1f6330f65df74278c0aa866b7d03f9c010d09
Travis Collins <travisfcollins@gmail.com>
2015-09-18 17:23:01 -0400

    added ubuntu installer script

---
+++ installDebs.sh
@@ -0,0 +1,21 @@
+apt-get install -y gcc g++ python
+apt-get install -y gcc g++ python python-dev
+apt-get install -y qt4-dev-tools
+apt-get install -y mercurial
+apt-get install -y bzr
+apt-get install -y cmake libc6-dev libc6-dev-i386 g++-multilib
+apt-get install -y gdb valgrind
+apt-get install -y gsl-bin libgsl0-dev libgsl0ldbl
+apt-get install -y flex bison libfl-dev
+apt-get install -y tcpdump
+apt-get install -y sqlite sqlite3 libsqlite3-dev
+apt-get install -y libxml2 libxml2-dev
+apt-get install -y libgtk2.0-0 libgtk2.0-dev
+#apt-get install -y vtun lxc # For vms
+apt-get install -y uncrustify
+apt-get install -y doxygen graphviz imagemagick
+apt-get install -y texlive texlive-extra-utils texlive-latex-extra texlive-font-utils
+apt-get install -y python-sphinx dia
+apt-get install -y python-pygraphviz python-kiwi python-pygoocanvas libgoocanvas-dev
+apt-get install -y libboost-signals-dev libboost-filesystem-dev
+apt-get install -y openmpi-bin openmpi-common openmpi-doc libopenmpi-dev
